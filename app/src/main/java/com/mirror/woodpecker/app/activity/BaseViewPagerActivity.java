44a02958d4c2e482723ee018488aa8f1691a9c54
mirror5821@163.com <mirror5821@163.com>
2016-03-01 18:01:27 +0800

    20160301

---
+++ app/src/main/java/com/mirror/woodpecker/app/activity/BaseViewPagerActivity.java
@@ -0,0 +1,108 @@
+package com.mirror.woodpecker.app.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.mirror.woodpecker.app.R;
+
+import dev.mirror.library.android.util.ScreenUtil;
+
+/**
+ * Created by 王沛栋 on 2016/3/1.
+ */
+public abstract class BaseViewPagerActivity extends BaseActivity {
+    private RadioGroup mRG;
+    public ViewPager mViewPager;
+    public RadioButton[] Rbs;
+
+    public  String [] Types;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_viewpager);
+
+        mRG = (RadioGroup)findViewById(R.id.view_select);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        Types = setTypes();
+
+        int typeLength = Types.length;
+
+        mViewPager.setOffscreenPageLimit(typeLength + 1);
+        mViewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int arg0) {
+                onPageChangeListenr(arg0);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+
+            }
+        });
+
+        Rbs = new RadioButton[typeLength];
+
+        int rbWidth = (ScreenUtil.getScreenWidth(BaseViewPagerActivity.this))/typeLength;
+        for(int i=0; i<typeLength; i++){
+            View v = getLayoutInflater().inflate(R.layout.view_tab_rb, null);
+
+            RadioButton tempButton =(RadioButton)v.findViewById(R.id.rb1);
+            tempButton.setText(Types[i]);
+            tempButton.setId(i);
+            Rbs[i] = tempButton;
+
+
+            mRG.addView(tempButton, rbWidth,LinearLayout.LayoutParams.MATCH_PARENT);
+        }
+
+        mRG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                onCheckChangeListener(group,checkedId);
+            }
+        });
+
+        Rbs[0].setChecked(true);
+
+    }
+
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int arg0) {
+            return setViewPagerFragment(arg0);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+    }
+
+
+    public abstract String[] setTypes();
+    public abstract void onPageChangeListenr(int position);
+    public abstract void onCheckChangeListener(RadioGroup group, int checkedId);
+    public abstract Fragment setViewPagerFragment(int position);
+
+}
